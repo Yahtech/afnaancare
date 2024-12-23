dd148da2fab6d526ac557a9cc05e471ce44e9be7
Yahtech <haneefahyahya2@gmail.com>
2024-12-22 19:17:29 -0800

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,7 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", function() {
-  const text = "At Afnaan Care, we are relentless in our resolve to make positive impact on the persons we care for and their families at large...Our core services include home care for the sick, incapacitated persons and others who may not be sick but require specialized care such as pregnant women, nursing mothers, infants and the aged.";
+  const text = "At Afnaan Care, we are relentless in our resolve to make positive impact on the persons we care for and their families at large...";
   const textElement = document.getElementById("liveTextt");
   let index = 0;
 
